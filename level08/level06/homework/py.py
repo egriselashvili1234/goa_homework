20ccc6249fb3c235199a5a527bf23828a1c4276f
egriselashvili1234 <nikoloziegriselashvili@gmail.com>
2024-06-08 10:50:25 +0400

    Add files via upload

---
+++ level08/level06/homework/py.py
@@ -0,0 +1,8 @@
+name = "nikoloz"
+surname = "egriselashvili"
+age = 11
+text = "I am programmer"
+
+print("My name is" + " " + name + " " + "my surname is" + " " + surname + " " + "my age is" + " " + str(age) + " " + text)
+
+#შევქმენი ცვლადები და დავუმატე ერთმანეთს
