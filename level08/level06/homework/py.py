c5f586e15df89d7ddc78050e3e2ed3872cd14553
egriselashvili1234 <nikoloziegriselashvili@gmail.com>
2024-06-08 13:04:20 +0400

    Delete level08 directory

--- level08/level06/homework/py.py
+++
@@ -1,8 +0,0 @@
-name = "nikoloz"
-surname = "egriselashvili"
-age = 11
-text = "I am programmer"
-
-print("My name is" + " " + name + " " + "my surname is" + " " + surname + " " + "my age is" + " " + str(age) + " " + text)
-
-#შევქმენი ცვლადები და დავუმატე ერთმანეთს
--- level08/level06/homework/py1.py
+++
@@ -1,6 +0,0 @@
-name = "nikoloz"
-surname = "egrishelashvili"
-age = 11
-text = "I am programmer"
-
-print("My name is" + " " + name + " " + "my surname is" + " " + surname + " " + "my age is" + " " + str(age) + " " + text)
